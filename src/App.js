b13b386ac1573cf284d149bc7ce088310a75d570
saqlain1020 <saqlainprinters@gmail.com>
2021-05-10 18:54:37 +0500

    Features done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Container } from '@material-ui/core';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Business from './Sections/Business/Business';
+import Features from './Sections/Features/Features';
 import Main from './Sections/Main/Main';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <Main/>
       <Business/>
+      <Features/>
     </Container>
   );
 }
